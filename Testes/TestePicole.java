244936bd5d91f9fdbd667d91168953c52cf311b3
Djair Alves <djair.dj@gmail.com>
2022-02-19 23:10:54 -0300

    Criação de Teste

---
+++ Testes/TestePicole.java
@@ -0,0 +1,10 @@
+package Testes;
+
+import Modelos.Picole;
+
+public class TestePicole {
+    public static void main(String[] args) {
+        System.out.println( "Testando o teste");
+        System.out.println(new Picole());
+    }
+}
